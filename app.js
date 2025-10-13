0a4d69653826d1fa07b6c28fef55542ce05f279b
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-10-13 13:10:38 +0800

    feat: create subdirectories needed and setup on app to use view engine, allowing to serve static files, and middleware for parsing form data

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+// If using ejs package, set the view engine to it
+app.set('view engine', 'ejs');
+
+// Allows serving of static files (i.e css)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded( {extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Homepage');
