51f553d0b3c26576431615732e3f0954ded43fa0
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-10-13 18:59:20 +0800

    feat: add basic setup for connecting to local db

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,10 @@
-exports.getHomepage = (req, res) => {
+const db = require('../db/queries');
+
+
+exports.getHomepage = async(req, res) => {
+  console.log(process.env);
+  const test = await db.getUsernames();
+  console.log("Test", test);
   res.render('pages/index', {
     title: 'Homepage'
   });
