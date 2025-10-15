1a2f61a26f362309a81f070f2e2f22673dcd3427
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-10-15 09:39:29 +0800

    docs: add comment on gamesController

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -2,6 +2,7 @@ const db = require('../db/queries');
 
 
 exports.gamesIndexGet = async(req, res) => {
+  // refactor later to search for games differently if there are query parameters on the url using req.query.queryName
   const games = await db.getGames();
   console.log("Games:", games);
   res.render('pages/games', {
